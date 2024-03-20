df2b219dd5c317d447c6c2222df053d24d40b22c
SeungHoon Han <101882530+sleepyhoon@users.noreply.github.com>
2024-03-20 17:34:19 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 152 ms, Memory: 14664 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,59 +1,58 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
-    static int n;
-    static int m;
-    static int[] cost;
-    static ArrayList<ArrayList<Integer>> arr;
-    static void bfs(int start){
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] chk = new boolean[n+1];
-        cost = new int[n+1];
-        q.offer(start);
-        chk[start] = true;
-        while(!q.isEmpty()){
-            int poll = q.poll();
-            for(int i=0;i<arr.get(poll).size();i++){
-                int cur = arr.get(poll).get(i);
-                if(!chk[cur]){
-                    q.offer(cur);
-                    cost[cur] = cost[poll] + 1;
-                    chk[cur] = true;
+import static java.lang.Math.min;
+
+public class Main{
+    static int n,m;
+    static int[][] rel;
+    static final int INF = 999999;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        rel = new int[n+1][n+1];
+        // 거리 초기화 하기
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==j) {
+                    rel[i][j] = 0;
+                }
+                else rel[i][j] = INF;
+            }
+        }
+        // 인접한 점끼리의 거리는 1이다.
+        for(int i=0;i<m;i++){
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            rel[from][to] = 1;
+            rel[to][from] = 1;
+        }
+        for(int k=1;k<=n;k++){
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=n;j++){
+                    rel[i][j] = min(rel[i][j],rel[i][k]+rel[k][j]);
                 }
             }
         }
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt(); // 유저의 수
-        m = sc.nextInt(); // 관계의 수
-        arr = new ArrayList<>();
-        // 인덱스 0 은 사용하지 않음.
-        for(int i=0;i<n+1;i++){
-            arr.add(new ArrayList<Integer>());
-        }
-        for(int i=0;i<m;i++){
-            int from = sc.nextInt();
-            int to = sc.nextInt();
-            arr.get(from).add(to);
-            arr.get(to).add(from);
-        }
-        // 케빈 베이컨이 가장 작은 학생의 번호
-        int answer = 0;
-        int min = Integer.MAX_VALUE;
-        // 학생은 1~n까지만 존재한다.
+        int result = 0;
+        int min = INF;
         for(int i=1;i<=n;i++){
-            bfs(i);
             int sum = 0;
-            // 코스트 출력
-            for (int i1 : cost) {
-                sum+=i1;
+            for(int j=1;j<=n;j++){
+                sum += rel[i][j];
             }
-            if(min>sum) {
+            if(min>sum){
                 min = sum;
-                answer = i;
+                result = i;
             }
         }
-        System.out.println(answer);
+        System.out.println(result);
     }
 }
