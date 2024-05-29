5b89ccb8ec436ea478f219ab0e623cbf8484e52b
SeungHoon Han <101882530+sleepyhoon@users.noreply.github.com>
2024-05-29 20:52:23 +0900

    [Silver V] Title: 집합, Time: 1132 ms, Memory: 312660 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,66 +1,32 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-    static ArrayList<Integer> list = new ArrayList<>(20);
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        while (n-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String s = st.nextToken();
-            if(s.equals("all") || s.equals("empty")) {
-                func(s);
-            }
+        StringBuilder sb = new StringBuilder();
+        int S = 0;
+        int M = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            if(str.equals("all")) S = (1 << 21) - 1;
+            else if(str.equals("empty")) S = 0;
             else {
-                int input = Integer.parseInt(st.nextToken());
-                func(s,input);
+                int num = Integer.parseInt(st.nextToken());
+                if(str.equals("add"))
+                    S |= (1 << num);
+                else if(str.equals("remove"))
+                    S &= ~(1 << num);
+                else if(str.equals("check"))
+                    sb.append((S & (1 << num)) != 0 ? 1 : 0).append("\n");
+                else if(str.equals("toggle"))
+                    S ^= (1 << num);
             }
         }
         System.out.println(sb);
-    }
-
-    private static void func(String s) {
-         if(s.equals("all")) {
-            ArrayList<Integer> tmp = new ArrayList<>();
-            for(int i=1;i<=20;i++) {
-                tmp.add(i);
-            }
-            list = tmp;
-        }
-        else {
-            list.clear();
-        }
-    }
-
-    static void func(String s,int a){
-        if(s.equals("add")) {
-            if(!list.contains(a)) {
-                list.add(a);
-            }
-        }
-        else if(s.equals("remove")) {
-            if(list.contains(a)) {
-                list.remove((Integer) a);
-            }
-        }
-        else if(s.equals("toggle")) {
-            if(list.contains(a)) {
-                list.remove((Integer) a);
-            }
-            else
-                list.add(a);
-        }
-        else if(s.equals("check")) {
-            if(list.contains(a)) {
-                sb.append(1).append("\n");
-            }
-            else {
-                sb.append(0).append("\n");
-            }
-        }
     }
 }
