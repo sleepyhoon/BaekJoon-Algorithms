6b4b079ead87c0bc141b9c6699d244ac5b67bc98
SeungHoon Han <101882530+sleepyhoon@users.noreply.github.com>
2024-05-29 18:18:57 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 700 ms, Memory: 59208 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int[] arr = new int[n+1];
@@ -20,11 +21,13 @@ public class Main {
             sum += arr[i];
             sumList[i] = sum;
         }
+        // 답 구하기 답 = sumList[end] - sumList[start-1]
         for(int i = 1; i <= m; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            System.out.println(sumList[end] - sumList[start-1]);
+            sb.append(sumList[end] - sumList[start-1]).append("\n");
         }
+        System.out.println(sb);
     }
 }
