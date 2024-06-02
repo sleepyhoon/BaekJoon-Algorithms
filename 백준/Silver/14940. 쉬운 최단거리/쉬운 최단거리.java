a54c9709cb690c587e6415c80260c8512f8e0cbc
SeungHoon Han <101882530+sleepyhoon@users.noreply.github.com>
2024-06-02 19:29:59 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 828 ms, Memory: 64584 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -55,11 +55,11 @@ public class Main {
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                System.out.print(cost[i][j]+" ");
+                sb.append(cost[i][j]+" ");
             }
-            System.out.println();
+            sb.append("\n");
         }
-
+        System.out.println(sb);
     }
 
     static void calculateCost(int row, int col,int depth){
