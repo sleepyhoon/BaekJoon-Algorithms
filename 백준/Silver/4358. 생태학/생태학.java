9e3368ccee3fe3618d038face8dc166e6eef1cc4
SeungHoon Han <pokjm99@gmail.com>
2024-10-12 15:14:07 +0900

    [Silver II] Title: 생태학, Time: 2964 ms, Memory: 86892 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<String,Integer> map = new HashMap<>();
+        ArrayList<String> list = new ArrayList<>();
+        double count = 0;
+        String s;
+        while ((s = br.readLine()) != null) {
+            map.put(s,map.getOrDefault(s,0) + 1);
+            if(!list.contains(s)) list.add(s);
+            count++;
+        }
+
+        // 사전순으로 나열해야 한다.
+        Collections.sort(list);
+
+        for(String a:list) {
+            double value = (double) map.get(a) / count * 100;
+            System.out.println(a + " " + String.format("%.4f",value));
+        }
+    }
+}
