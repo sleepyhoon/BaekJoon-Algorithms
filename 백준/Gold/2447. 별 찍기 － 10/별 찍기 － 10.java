9409c3c678a111cbdc18a71218f0c47e719b47ca
SeungHoon Han <pokjm99@gmail.com>
2024-12-26 14:43:33 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 348 ms, Memory: 51036 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,49 +1,49 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    static String[][] arr;
+    static char[][] arr;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
         int n = sc.nextInt();
-        arr = new String[n][n];
-        func(0,0,n);
+        arr = new char[n][n];
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if (arr[i][j] == null) {
-                    sb.append(" ");
-                } else {
-                    sb.append(arr[i][j]);
-                }
+        func(0,0,n,false);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(arr[i][j]);
             }
-            sb.append("\n");
+            sb.append('\n');
         }
         System.out.println(sb);
     }
 
-    private static void func(int x,int y, int n) {
-        if(n==3) {
+    private static void func(int x,int y, int n,boolean blank) {
+        if(blank) {
             for(int i=x;i<x+n;i++){
                 for(int j=y;j<y+n;j++){
-                    if(i==x+1&&j==y+1)
-                        arr[i][j] = " ";
-                    else
-                        arr[i][j] = "*";
+                    arr[i][j] = ' ';
                 }
             }
+            return;
         }
-        else {
-            int newSize = n/3;
-            func(x,y,newSize);
-            func(x,y+newSize,newSize);
-            func(x,y+2*newSize,newSize);
-            func(x+newSize,y,newSize);
-            func(x+2*newSize,y,newSize);
-            func(x+newSize,y+2*newSize,newSize);
-            func(x+2*newSize,y+newSize,newSize);
-            func(x+2*newSize,y+2*newSize,newSize);
+        if(n==1) {
+            arr[x][y] = '*';
+            return;
+        }
+
+        int newSize = n/3;
+        int count = 0;
+
+        for(int i=x;i<x+n;i+=newSize) {
+            for(int j=y;j<y+n;j+=newSize) {
+                count++;
+                if(count==5)
+                    func(i,j,newSize,true);
+                else
+                    func(i,j,newSize,false);
+            }
         }
     }
 }
