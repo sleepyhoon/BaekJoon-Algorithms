62ec88fa2afc5a27e17fb115127759436da08702
SeungHoon Han <pokjm99@gmail.com>
2025-01-14 18:33:17 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,6 +1,8 @@
 -- 코드를 입력하세요
 SELECT I.NAME, I.DATETIME
 from ANIMAL_INS as I
-where I.ANIMAL_ID not in (select O.ANIMAL_ID from ANIMAL_OUTS as O)
+    left join ANIMAL_OUTS as O
+    on I.ANIMAL_ID = O.ANIMAL_ID 
+where O.ANIMAL_ID is null
 order by I.datetime asc
 limit 3;
