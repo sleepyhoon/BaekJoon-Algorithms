1aeb7484c05b062e15e1779f0939dfa35d8d536f
SeungHoon Han <pokjm99@gmail.com>
2025-01-21 21:58:54 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1404 ms, Memory: 125680 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,71 +1,69 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.*;
-
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arr;
-
-    static int lowerBound(int target) {
-        int left = 0;
-        int right = arr.length;
-
-        while(left<right){
-            // 중간 위치를 구한다.
-            int mid = (left + right) / 2;
-            // 중복 원소에 대해서 왼쪽으로 탐색하도록 상계를 내린다.
-            if (target<=arr[mid])
-                right = mid;
-            else
-                left = mid + 1;
-        }
-        return left;
-    }
-
-    static int upperBound(int target) {
-        int left = 0;
-        int right = arr.length;
-
-        while(left<right){
-            int mid = (left + right) / 2;
-
-            if(target<arr[mid]){
-                right = mid;
-            }
-            else {
-                left = mid + 1;
-            }
-        }
-        return left;
-    }
-
+    static int[] a,b;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        // 상근이가 가진 카드 입력하기
-        int have = Integer.parseInt(br.readLine());
-        arr = new int[have];
+        int n = Integer.parseInt(br.readLine());
+        a = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0;i<have;i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+        for(int i=0;i<n;i++){
+            a[i] = Integer.parseInt(st.nextToken());
         }
-        // 몇개있는지 찾고 싶은 카드들 입력하기
-        int find = Integer.parseInt(br.readLine());
-        int[] arr2 = new int[find];
-        st = new StringTokenizer(br.readLine());
-        for(int i=0;i<find;i++){
-            arr2[i] = Integer.parseInt(st.nextToken());
-        }
-        // 상근이가 가지고 있는 카드 오름차순으로 정렬
-        Arrays.sort(arr);
-        // 상근이가 해당 카드를 몇 개 가지고 있는지 구하기
-        for(int i=0;i<find;i++) {
-            int target = arr2[i];
 
-            sb.append(upperBound(target)-lowerBound(target)).append(' ');
+        int m = Integer.parseInt(br.readLine());
+        b = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<m;i++){
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(a);
+
+        for(int i=0;i<m;i++) {
+            sb.append(upperBound(b[i]) - lowerBound(b[i])).append(" ");
         }
         System.out.println(sb);
+    }
+
+    private static int lowerBound(int target) {
+        int left = 0;
+        int right = a.length;
+
+        while(left < right) {
+            int mid = left + ((right - left) / 2);
+
+            if(a[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    private static int upperBound(int target) {
+        int left = 0;
+        int right = a.length;
+
+        while(left < right) {
+            int mid = left + ((right - left) / 2);
+
+            if(a[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
     }
 }
