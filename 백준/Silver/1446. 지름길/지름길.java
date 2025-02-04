69e80fe252e1bda9c3a2a9c1d133ddb5816da5e7
SeungHoon Han <pokjm99@gmail.com>
2025-02-04 16:15:07 +0900

    [Silver I] Title: 지름길, Time: 108 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.java
+++ 백준/Silver/1446. 지름길/지름길.java
@@ -33,24 +33,16 @@ public class Main {
             int cost = Integer.parseInt(st.nextToken());
             arr[i] = new Road(from, to, cost);
         }
-
-        Arrays.sort(arr, (o1, o2) -> {
-            if (o1.to == o2.to) {
-                return o1.from - o2.from;
-            } else {
-                return o1.to - o2.to;
-            }
-        });
-        Arrays.fill(dp,Integer.MAX_VALUE);
+        Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
         for (int i = 1; i <= d; i++) {
             for (Road road : arr) {
                 if (i == road.to) {
-                    dp[i] = Math.min(Math.min(dp[i-1]+1,dp[i]), dp[road.from] + road.cost);
+                    dp[i] = Math.min(Math.min(dp[i - 1] + 1, dp[i]), dp[road.from] + road.cost);
                 }
             }
-            if(dp[i] == Integer.MAX_VALUE) {
-                dp[i] = dp[i-1] +1;
+            if (dp[i] == Integer.MAX_VALUE) {
+                dp[i] = dp[i - 1] + 1;
             }
         }
         System.out.println(dp[d]);
