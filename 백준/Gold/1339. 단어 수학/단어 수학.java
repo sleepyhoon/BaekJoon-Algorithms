145d635888824c497be03e3e5d50ff3f10cbecc4
SeungHoon Han <pokjm99@gmail.com>
2025-02-19 16:00:04 +0900

    [Gold IV] Title: 단어 수학, Time: 172 ms, Memory: 17772 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -16,42 +16,40 @@ public class Main {
             this.w = w;
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
         List<String> list = new ArrayList<>();
         List<Alphabet> arr = new ArrayList<>();
-        Map<Character,Integer> map = new HashMap<>();
-        Map<Character,Integer> weights = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> weights = new HashMap<>();
         for (int i = 0; i < n; i++) {
             String input = sc.next();
-            for(int j=0;j<input.length();j++){
-                double pow = Math.pow(10, input.length() - 1 - j);
-                if(!map.containsKey(input.charAt(j))) {
-                    map.put(input.charAt(j),(int) pow);
-                } else {
-                    map.put(input.charAt(j),map.get(input.charAt(j)) +(int) pow);
-                }
+            for (int j = 0; j < input.length(); j++) {
+                char key = input.charAt(j);
+                int pow = (int) Math.pow(10, input.length() - 1 - j);
+                map.put(key, map.getOrDefault(key, 0) + pow);
             }
             list.add(input);
         }
 
         for (Entry<Character, Integer> entry : map.entrySet()) {
-            arr.add(new Alphabet(entry.getKey(),entry.getValue()));
+            arr.add(new Alphabet(entry.getKey(), entry.getValue()));
         }
 
-        Collections.sort(arr,(o1,o2) -> o2.w - o1.w);
+        Collections.sort(arr, (o1, o2) -> o2.w - o1.w);
         int value = 9;
-        for(Alphabet a : arr) {
-            weights.put(a.c,value--);
+        for (Alphabet a : arr) {
+            weights.put(a.c, value--);
         }
 
         int sum = 0;
 
         for (String s : list) {
-            for(int i=0;i<s.length();i++){
-                sum += weights.get(s.charAt(i)) * (int) Math.pow(10,s.length()-1-i);
+            for (int i = 0; i < s.length(); i++) {
+                sum += weights.get(s.charAt(i)) * (int) Math.pow(10, s.length() - 1 - i);
             }
         }
         System.out.println(sum);
