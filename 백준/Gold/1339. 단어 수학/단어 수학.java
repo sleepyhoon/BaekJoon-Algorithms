93cb7b86b346ba3b5374600b3090a105e5b26ede
SeungHoon Han <pokjm99@gmail.com>
2025-02-19 15:56:51 +0900

    [Gold IV] Title: 단어 수학, Time: 184 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+public class Main {
+    static class Alphabet {
+        char c;
+        int w;
+
+        Alphabet(char c, int w) {
+            this.c = c;
+            this.w = w;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        List<String> list = new ArrayList<>();
+        List<Alphabet> arr = new ArrayList<>();
+        Map<Character,Integer> map = new HashMap<>();
+        Map<Character,Integer> weights = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String input = sc.next();
+            for(int j=0;j<input.length();j++){
+                double pow = Math.pow(10, input.length() - 1 - j);
+                if(!map.containsKey(input.charAt(j))) {
+                    map.put(input.charAt(j),(int) pow);
+                } else {
+                    map.put(input.charAt(j),map.get(input.charAt(j)) +(int) pow);
+                }
+            }
+            list.add(input);
+        }
+
+        for (Entry<Character, Integer> entry : map.entrySet()) {
+            arr.add(new Alphabet(entry.getKey(),entry.getValue()));
+        }
+
+        Collections.sort(arr,(o1,o2) -> o2.w - o1.w);
+        int value = 9;
+        for(Alphabet a : arr) {
+            weights.put(a.c,value--);
+        }
+
+        int sum = 0;
+
+        for (String s : list) {
+            for(int i=0;i<s.length();i++){
+                sum += weights.get(s.charAt(i)) * (int) Math.pow(10,s.length()-1-i);
+            }
+        }
+        System.out.println(sum);
+    }
+}
