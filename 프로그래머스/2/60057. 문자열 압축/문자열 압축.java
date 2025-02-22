1457c6da3c2166d65bb50d84a187dc9a12598f21
SeungHoon Han <pokjm99@gmail.com>
2025-02-22 14:30:45 +0900

    [level 2] Title: 문자열 압축, Time: 5.85 ms, Memory: 95.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int solution(String s) {
+        int length = 1;
+        int minLength = 10000;
+        while (length <= s.length()) {
+            int start = 0;
+            String previous = "";
+            int count = 1;
+            StringBuilder sb = new StringBuilder();
+            while (start < s.length()) {
+                String sub;
+                if(start + length >= s.length()) {
+                    sub = s.substring(start);
+                } else {
+                    sub = s.substring(start, start + length);
+                }
+                if (previous.isBlank()) {
+                    previous = sub;
+                } else if (previous.equals(sub)) {
+                    count++;
+                } else {
+                    if (count > 1) {
+                        sb.append(count);
+                    }
+                    sb.append(previous);
+                    previous = sub;
+                    count = 1;
+                }
+                start += length;
+            }
+            if(count > 1) {
+                sb.append(count).append(previous);
+            } else {
+                sb.append(previous);
+            }
+            minLength = Math.min(minLength, sb.length());
+            length++;
+        }
+
+        return minLength;
+    }
+}
