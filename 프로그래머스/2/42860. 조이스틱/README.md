91563a92fb0bd6b1f67c718317cdde7916f6990b
SeungHoon Han <pokjm99@gmail.com>
2025-03-25 13:41:29 +0900

    [level 2] Title: 조이스틱, Time: 0.10 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
 
 ### 성능 요약
 
-메모리: 70.4 MB, 시간: 0.07 ms
+메모리: 70.8 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 25일 12:41:06
+2025년 03월 25일 13:41:26
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -3,28 +3,20 @@ class Solution {
         int answer = 0;
         int length = name.length();
 
-        // 1. 상하 조작 횟수 계산 (각 문자마다 A에서 목표 문자까지의 최소 이동)
-        for (int i = 0; i < length; i++) {
-            char c = name.charAt(i);
-            int move = Math.min(c - 'A', 'Z' - c + 1);
-            answer += move; 
+        for(int i=0;i<length;i++){
+            char ch = name.charAt(i);
+            answer += Math.min(ch - 'A', 'Z' - ch + 1);
         }
-
-        // 2. 좌우 커서 이동 최적화
-        int move = length - 1; // 기본: 오른쪽으로 쭉 가는 경우
-        for (int i = 0; i < length; i++) {
-            int next = i + 1;
-
-            // 연속된 A 구간의 끝까지 이동
-            while (next < length && name.charAt(next) == 'A') {
+        
+        int move = length - 1;
+        for(int i=0;i<length;i++){
+            int next = i+1;
+            while(next < length && name.charAt(next) == 'A') {
                 next++;
             }
-
-            // 좌로 갔다가 돌아오는 경우, 우로 갔다가 돌아오는 경우 중 최소
-            move = Math.min(move, (i * 2) + length - next);
-            move = Math.min(move, (length - next) * 2 + i);
+            move = Math.min(move,(2*i) + length - next);
+            move = Math.min(move, 2*(length-next) + i);
         }
-
         answer += move;
         return answer;
     }
